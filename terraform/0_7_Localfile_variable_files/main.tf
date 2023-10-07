742dcdcbbe2a9abd4268c44c63aea125a1caedc4
Saxena, Ashwani <ashwani_saxena@optum.com>
2023-10-07 22:37:42 +0530

    update

---
+++ terraform/0_7_Localfile_variable_files/main.tf
@@ -0,0 +1,10 @@
+resource "local_file" "pet" {
+    content = var.content
+    filename = var.filename
+}
+
+resource "random_pet" "my_pet" {
+    prefix = var.prefix
+    separator = var.separator
+    length = var.length
+}
