257373435ccdc025cfce3b7ff311a24fc7184382
Saxena, Ashwani <ashwani_saxena@optum.com>
2023-10-18 08:12:20 +0530

    update

---
+++ terraform/0_AWS_S3_BucketCreating_savefile/main.tf
@@ -0,0 +1,55 @@
+#terraform init
+#terraform plan
+#terraform apply
+
+# Configure the AWS Provider version
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.21"
+    }
+  }
+}
+
+# Configure the AWS s3 bucket
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+  #access_key = "my-access-key"
+  #secret_key = "my-secret-key"
+}
+# creating s3 bucket
+resource "aws_s3_bucket" "s3" {
+  bucket = "aws-s3-bucket991"
+}
+
+
+# enable version
+resource "aws_s3_bucket_versioning" "s3_version" {
+  bucket = aws_s3_bucket.s3.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+# creating file 
+resource "aws_s3_object" "s3_file" {
+  bucket = aws_s3_bucket.s3.bucket
+  key    = "document.doc"
+  source = "./document.doc"
+}
+
+
+
+#check before get output
+#terraform output
+# > aws_s3_bucket.s3
+# > aws_s3_bucket.s3.versioning
+/* output "Verify_version" {
+    value = aws_s3_bucket.s3.versioning[0].enabled
+}
+
+output "Verify_complete" {
+    value = aws_s3_bucket.s3
+} */
