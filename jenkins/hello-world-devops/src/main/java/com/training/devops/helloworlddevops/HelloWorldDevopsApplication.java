643ae9ab976ace95888cc198b2f1626da8d92e13
Saxena, Ashwani <ashwani_saxena@optum.com>
2023-11-15 07:57:15 +0530

    Update

---
+++ jenkins/hello-world-devops/src/main/java/com/training/devops/helloworlddevops/HelloWorldDevopsApplication.java
@@ -0,0 +1,12 @@
+package com.training.devops.helloworlddevops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldDevopsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorldDevopsApplication.class, args);
+	}
+}
